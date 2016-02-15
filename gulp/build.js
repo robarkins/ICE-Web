b5108702885eddc0186dd00a1470703555d5d3b4
robarkins <robarkins1987@gmail.com>
2016-02-15 14:23:19 +0000

    Changed module name in gulp build file

--- gulp/build.js
+++ gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'inspinia'
+      module: 'iceWeb'
     }))
     .pipe(gulp.dest(paths.tmp + '/partials/'));
 });
